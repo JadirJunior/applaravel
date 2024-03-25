b1fc22991649cb86afefce63668ecc226584c5e7
JadirJunior <jadirjunior8@gmail.com>
2024-03-25 11:52:10 -0300

    Form Styles

--- resources/views/autor/__form.blade.php
+++ resources/views/autor/__form.blade.php
@@ -1,96 +1,117 @@
-<div>    
-    <div>
-            <Label for="">Nome</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="nome"
-                id="nome"
-                value="{{ isset($registro->nome) ? $registro->nome : ''}}">
-                @error('nome')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-        <div>
-            <Label for="">Apelido</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="apelido"
-                id="apelido"
-                value="{{ isset($registro->apelido) ? $registro->apelido : ''}}">
-                @error('apelido')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-        <div>
-            <Label for="">Cidade</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="cidade"
-                id="cidade"
-                value="{{ isset($registro->cidade) ? $registro->cidade : ''}}">
-                @error('cidade')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-        <div>
-            <Label for="">Bairro</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="bairro"
-                id="bairro"
-                value="{{ isset($registro->bairro) ? $registro->bairro : ''}}">
-                @error('bairro')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-        <div>
-            <Label for="">CEP</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="cep"
-                id="cep"
-                value="{{ isset($registro->cep) ? $registro->cep : ''}}">
-                @error('cep')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-        <div>
-            <Label for="">E-mail</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="email"
-                id="email"
-                value="{{ isset($registro->email) ? $registro->email : ''}}">
-                @error('email')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-        <div>
-            <Label for="">Telefone</Label>
-            <!-- isset é para validar se o campo é nulo -->
-            <input type="text"
-                name="telefone"
-                id="telefone"
-                value="{{ isset($registro->telefone) ? $registro->telefone : ''}}">
-                @error('telefone')
-                    <span>
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-        </div>
-</div>    
+<div>
+    <div class="mb-3">
+        <Label for="" class="form-label">Nome</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="nome" 
+            id="nome" 
+            value="{{ isset($registro->nome) ? $registro->nome : ''}}"
+            placeholder="Nome">
+        @error('nome')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <Label for="" class="form-label">Apelido</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="apelido" 
+            id="apelido" 
+            value="{{ isset($registro->apelido) ? $registro->apelido : ''}}"
+            placeholder="Apelido">
+        @error('apelido')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <Label for="" class="form-label">Cidade</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="cidade" 
+            id="cidade" 
+            value="{{ isset($registro->cidade) ? $registro->cidade : ''}}"
+            placeholder="Cidade">
+        @error('cidade')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <Label for="" class="form-label">Bairro</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="bairro" 
+            id="bairro" 
+            value="{{ isset($registro->bairro) ? $registro->bairro : ''}}"
+            placeholder="Bairro">
+        @error('bairro')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <Label for="" class="form-label">CEP</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="cep" 
+            id="cep" 
+            value="{{ isset($registro->cep) ? $registro->cep : ''}}"
+            placeholder="CEP">
+        @error('cep')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <Label for="" class="form-label">E-mail</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="email" 
+            id="email" 
+            value="{{ isset($registro->email) ? $registro->email : ''}}"
+            placeholder="Email">
+        @error('email')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <Label for="" class="form-label">Telefone</Label>
+        <!-- isset é para validar se o campo é nulo -->
+        <input 
+            class="form-control" 
+            type="text" 
+            name="telefone" 
+            id="telefone" 
+            value="{{ isset($registro->telefone) ? $registro->telefone : ''}}"
+            placeholder="Telefone">
+        @error('telefone')
+        <span>
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+</div>
 
 
 <!-- <pre>
